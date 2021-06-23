9fbe36b82c0cece3ad4535e58aa321ac7ce8931a
Sam Swartzberg <me@samswartzberg.com>
2021-06-23 21:48:50 +0200

    add basic cache functionality

--- src/cache.rs
+++ src/cache.rs
@@ -1,5 +1,18 @@
-fn get_result_from_cache() -> Result<???,&str>{ 
- 
+fn get_cache_location() -> &str {
+  std::env::var("XDG_CACHE_HOME").unwrap_or_else(|| std::env::var("HOME").unwrap() + ".cache") + "/tenki.json"
+}
+
+fn get_result_from_cache(cache_duration: chrono::Duration) -> Option<&str>{ 
+  if let Ok(metadata) = std::fs::metadata(get_cache_location()){
+    if let Ok(time) = metadata.created(){
+      let file_creation_time = chrono::DateTime<Local>.from(time);
+      if file_creation_time.checked_add_signed(cache_duration) > chrono::Local::now() {
+        Some(std::fs::read_to_string(get_cache_location()).ok())
+      } else { None }
+    } else {None }
+  } else { None }
 } 
  
-
+fn cache_result(result: &str) {
+  std::fs::write(get_cache_location(), result);
+}
