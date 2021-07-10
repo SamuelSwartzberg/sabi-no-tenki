89faa86bc3dbef096aa9c1d6f23e5015ab2a9a6d
Sam Swartzberg <me@samswartzberg.com>
2021-07-10 05:34:00 +0200

    Fix #10 by switching to clap 3.0.0 (beta)

--- src/input.rs
+++ src/input.rs
@@ -7,7 +7,7 @@ use crate::prog_options::ProgOptions;
 use parse_strings::{parse_location_list, parse_time, parse_metric_vector, parse_significant_figures, parse_cache_duration};
 use config::ConfigView;
 
-fn check_clap_boolean_flags(flag_names: [&str; 6], clap_matches: &clap::ArgMatches<'static>) -> Vec<bool>{
+fn check_clap_boolean_flags(flag_names: [&str; 6], clap_matches: &clap::ArgMatches) -> Vec<bool>{
   flag_names.into_iter().map(|flag_name| clap_matches.is_present(flag_name)).collect()
 }
 
@@ -19,7 +19,7 @@ fn replace_with_config(config: ConfigView, options: &mut ProgOptions){
   if let Some(res) = config.value_of("cache_duration") {options.cache_duration = parse_cache_duration(res)}; 
   if let Some(res) = config.value_of("metrics") {options.metrics = parse_metric_vector(res)};
 }
-fn replace_with_arguments(clap_matches: clap::ArgMatches<'static>, options: &mut ProgOptions){
+fn replace_with_arguments(clap_matches: clap::ArgMatches, options: &mut ProgOptions){
   let bool_flags = check_clap_boolean_flags(["emoji", "text", "human_readable", "week_starts_sat", "week_starts_sun", "labeled_columns"], &clap_matches);
   options.emoji = bool_flags[0]; options.text = bool_flags[1]; options.human_readable = bool_flags[2]; options.week_starts_sat = bool_flags[3]; options.week_starts_sun = bool_flags[4]; options.labeled_columns = bool_flags[5];
   if let Some(res) = clap_matches.value_of("location_list").map(String::from) { options.location_list = parse_location_list(res)};
@@ -32,7 +32,7 @@ fn replace_with_arguments(clap_matches: clap::ArgMatches<'static>, options: &mut
   //options.api =  clap_matches.value_of("api").map_or(options.api, |api_str| api::get_api(api_str).expect(ErrorStrings::NoSuchApi.get_message().unwrap())); 
 }
 
-pub fn parse_matches_into_options(clap_matches: clap::ArgMatches<'static>) -> ProgOptions{
+pub fn parse_matches_into_options(clap_matches: clap::ArgMatches) -> ProgOptions{
   let mut options = defaults::get_base_defaults();
 
   if let Some(config) = config::get_config_view() { replace_with_config(config, &mut options)}; 
